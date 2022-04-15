130e44f08521e687367f2dd8d1cf54f98cbc3f5a
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-04-15 14:22:51 +0900

    urls bookmark:add

---
+++ bookmark/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from bookmark.views import BookmarkListView, BookmarkCreateView
+
+app_name = 'bookmark'
+
+urlpatterns = [
+    path('list/', BookmarkListView.as_view(), name='list') , #bookmark:list
+    path('add/', BookmarkCreateView.as_view(), name='Add')  #bookmark:list
+]
