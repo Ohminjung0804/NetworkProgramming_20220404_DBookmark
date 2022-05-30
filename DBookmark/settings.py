192b3b971c6326503d5b747cb578b2e2819d33a4
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-05-30 14:19:43 +0900

    설정

--- DBookmark/settings.py
+++ DBookmark/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-dez$tkm38w8$(4j9z_odz+3e_1v&^z55k4ya=tfeu(q96pst3$'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-dez$tkm38w8$(4j9z_odz+3e_1v&^z55k4ya=tfeu(q96pst3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
-
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
