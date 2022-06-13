c2cca2c24c0f6ed36bd7b8cb8079ac13ed4beb67
Ohminjung0804 <w2009@e-mirim.hs.kr>
2022-06-13 14:44:43 +0900

    bookmark/views BookmarkListView get_queryset(),BookmarkCreateView get_initial()

--- bookmark/views.py
+++ bookmark/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
+from accounts.models import Profile
 from bookmark.models import Bookmark
 
 
@@ -11,13 +12,24 @@ class BookmarkListView(ListView):
     model = Bookmark
     #bookmark_list.html, {'bookmark_list': Bookmark.objects.all()}
 
-
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            profile = Profile.objects.get(user = user)
+            bookmark_list = Bookmark.objects.filter(profile=profile)
+        else:
+            bookmark_list = Bookmark.objects.none()
+        return bookmark_list
 class BookmarkCreateView(CreateView):
     model = Bookmark
-    fields = ['name', 'url']    #'__all__'
+    fields = ['prifuke','name', 'url']    #'__all__'
     template_name_suffix = '_create'    #bookmark_form.html -> bookmark_create.html
     success_url = reverse_lazy('bookmark:list')
 
+    def get_initial(self):
+        user = self.request.user
+        profile = Profile.objects.get(user = user)
+        return{'profile':profile}
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
